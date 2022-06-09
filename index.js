0dc0171a18e90d68666e391188a18da27b02ba38
Gaelle Briet <gaelle.briet@protonmail.com>
2022-06-09 21:35:39 +0200

    exercice 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ===== Exercice 1 ===== //
 /* Écrire un algorithme permettant de faire la somme des N premiers
- entiers. La fonction demandée prend en entrée un nombre entier, N, et
- renvoie un autre nombre entier, la somme demandée.*/
+entiers. La fonction demandée prend en entrée un nombre entier, N, et
+renvoie un autre nombre entier, la somme demandée.*/
 let somme = 0;
 
 const sommeEntiers = (number) => {
@@ -17,3 +17,42 @@ function sommeEntiers2(number) {
   console.log(somme);
   somme = 0;
 }
+
+// ===== Exercice 2 ===== //
+/* Ecrire un algorithme qui vous demande d'entrer une phrase et qui affiche cette phrase à l’envers.*/
+
+// split() place chaque caractère de la chaîne en un tableau
+// reverse() inverse les elements d'un tableau
+// join() rejoint les caractères
+
+const inversePhrase = (inv) => {
+  return inv.split("").reverse().join("");
+};
+
+let phrase = inversePhrase("je suis inversé");
+console.log(phrase);
+
+//---
+let phrase2 = "Je suis une phrase";
+let reverse = phrase2.split("").reverse().join("");
+console.log(reverse);
+
+//----
+function inverseMot(str) {
+  str = str.split("").reverse().join("");
+  return str;
+}
+
+str = "je vais etre à l'envers";
+console.log(inverseMot(str));
+
+//---
+// boucle for
+function reversion(s) {
+  let stringInverse = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    stringInverse += s[i];
+  }
+  return stringInverse;
+}
+console.log(reversion("moi aussi"));
